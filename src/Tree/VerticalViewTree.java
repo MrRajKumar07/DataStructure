cc5aa7bad2af7dbefbd78ccb4a817067e7a50906
MrRandhir07 <rajkumar951ce@gmail.com>
2025-08-02 13:02:30 +0530

    Tree

---
+++ src/Tree/VerticalViewTree.java
@@ -0,0 +1,15 @@
+package Tree;
+
+import java.util.*;
+
+class TreeNode1{
+    int val;
+    TreeNode1 left,right;
+    TreeNode1(int data){
+        val=data;
+        left=right=null;
+    }
+}
+public class VerticalViewTree {
+    
+}
